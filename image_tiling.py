0c555469636f79d6f11096a0d3ce9406ebb4799a
Ben North <ben@redfrontdoor.org>
2016-02-21 09:31:14 +0000

    padded_size(): Add

--- image_tiling.py
+++ image_tiling.py
@@ -12,3 +12,15 @@ def round_up_to_multiple(n, d):
 
 def tile_stride(viewport_half_size, overlap):
     return 2 * viewport_half_size - overlap
+
+def padded_size(raw_size, viewport_half_size, overlap):
+    """
+    Return the size which we must pad the original image to (along one
+    dimension) such that an integer number of overlapping tiles cover
+    it.
+    """
+    stride = tile_stride(viewport_half_size, overlap)
+    viewport_size = 2 * viewport_half_size
+    clamped_below_size = max(raw_size, viewport_size)
+    return (viewport_size
+            + round_up_to_multiple(clamped_below_size - viewport_size, stride))
